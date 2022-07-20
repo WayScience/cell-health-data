e58534d9fa2fb8ab63c4ee8879e66d6d7bfdc676
roshankern <roshan.kern@ucdenver.edu>
2022-07-20 11:13:24 -0600

    add segmentation module
    Co-authored-by: Jenna Tomkinson <jenna.tomkinson@ucdenver.edu>

---
+++ 1.segment-data/README.md
@@ -0,0 +1,55 @@
+# Segment Cell Painting Images
+
+In this module, we present our pipeline for segmenting nuclei and cyoplasm from the Cell Health data.
+
+### Segmentation
+
+We use the CellPose segmentation algorithim to segment the nuclei from each mitosis movie. 
+CellPose was first introduced in [Stringer, C., Wang, T., Michaelos, M. et al., 2020](https://doi.org/10.1038/s41592-020-01018-x) and we use the [python implementation](https://github.com/mouseland/cellpose).
+
+Stringer et al. trained the CellPose segmentation models on a diverse set of cell images and is therefore a good selection for our use case.
+The CellPose python implementation was particularly useful for building reproducible pipelines.
+
+After manually experiementing with CellPose on about 10 Cell Health nuclei images (various cell lines), we settled on the following parameters for CellPose nuclei segmentation:
+- `model_type = "cyto"` This parameter forces CellPose to use the cytoplasm model, which we found segments nuclei in the Cell Health data significantly better than the nucleus model. 
+More information about CellPose models can be found at https://cellpose.readthedocs.io/en/latest/models.html.
+- `channels = [0,0]` This parameter forces the model to segment cells in grayscale (in the case of Cell Health data single channel images).
+- `diameter = 80` This parameter indicates to the model that the average cell diameter is 80 pixels.
+- `flow_threshold=0` This paramenter decreases the maximum allowed error of the flows for each mask (default is `flow_threshold=0.4`).
+More information about CellPose settings can be found at https://cellpose.readthedocs.io/en/latest/settings.html.
+
+After manually experiementing with CellPose on about 10 Cell Health cytoplasm images (various cell lines), we settled on the following parameters for CellPose cytoplasm segmentation:
+- `model_type = "cyto"` This parameter forces CellPose to use the cytoplasm model for segmentation.
+More information about CellPose models can be found at https://cellpose.readthedocs.io/en/latest/models.html.
+- `channels = [1,3]` This parameter forces the model to segment cells using the nuclei and RNA channels of cell painting images. We overlay nuclei, ER, and RNA channels for CellPose with our `overlay_images()` function in [segment-cell-health-data.ipynb](segment-cell-health-data.ipynb).
+- `diameter = 0` This parameter forces the CellPose model to estimate the diameter of cells being segmented.
+- `flow_threshold=0` This paramenter decreases the maximum allowed error of the flows for each mask (default is `flow_threshold=0.4`).
+- `cellprob_threshold=0.4` This parameter is used to determine ROIs (default is `cellprob_threshold=0`). More information about CellPose settings can be found at https://cellpose.readthedocs.io/en/latest/settings.html.
+
+## Step 1: Setup Segmentation Environment
+
+### Step 1a: Create Segmentation Environment
+
+```sh
+# Run this command to create the conda environment for Segmentation data
+conda env create -f 1.segment-env.yml
+```
+
+### Step 1b: Activate Segmentation Environment
+
+```sh
+# Run this command to activate the conda environment for Segmentation data
+conda activate 1.segment-data-cell-health
+```
+
+### Step 1c (Optional): Complete PyTorch GPU Setup
+
+If you would like use PyTorch GPU when using CellPose, follow [these instructions](https://github.com/MouseLand/cellpose#gpu-version-cuda-on-windows-or-linux) to complete the PyTorch GPU setup.
+We use PyTorch GPU while segmenting Cell Health data.
+
+## Step 2: Execute Training Data Segmentation
+
+```bash
+# Run this script to segment Cell Health data
+bash 1.segment_data.sh
+```
---
+++ 1.segment-data/segment-cell-health-data.py
@@ -0,0 +1,203 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Cell Health Data Segmentor
+# ### Find center coordinates for cells from Cell Health data
+# 
+# #### Import libraries
+
+# In[1]:
+
+
+# from cellpose.io import logger_setup
+from cellpose import models, core, io, utils
+
+import pathlib
+import pandas as pd
+
+import cv2
+import numpy as np
+
+
+# ### Set Up CellPose
+
+# In[2]:
+
+
+use_GPU = core.use_gpu()
+print(">>> GPU activated? %d" % use_GPU)
+# logger_setup();
+
+
+# ### Helper functions for segmenting Cell Health data
+
+# In[3]:
+
+
+def overlay_channels(current_image: str, current_dir: pathlib.Path) -> np.ndarray:
+    """overlays nuclei, ER, and RNA channels to help with CellPose segmentation
+
+    Args:
+        current_image (str): string of current field to overlay channels for
+        current_dir (pathlib.Path): directory of current field
+
+    Returns:
+        np.ndarray: 3 channel image with nuclei, ER, RNA
+    """
+    # load all channel images
+    channel_paths = []
+    channel_images = []
+    for image_file in current_dir.iterdir():
+        if current_image in image_file.name:
+            channel_paths.append(image_file)
+    channel_paths = sorted(channel_paths)
+    for channel_path in channel_paths:
+        channel_image = np.array(cv2.imread(str(channel_path), 0))
+        channel_images.append(channel_image)
+
+    overlay = np.dstack(
+        [channel_images[0] * 2, channel_images[1] * 0, channel_images[2] * 5]
+    ).astype(np.uint8)
+    return overlay
+
+
+def get_cytoplasm_locations(
+    overlay_image: np.ndarray, cellpose_model: models.Cellpose
+) -> pd.DataFrame:
+    """finds center X,Y of cytoplasm and saves as tsv file
+
+    Args:
+        overlay_image (np.ndarray): overlay image with nuclei and RNA channels
+        cellpose_model (models.Cellpose): cellpose model for segmenting nuclei
+
+    Returns:
+        pd.DataFrame: dataframe with cytoplasm center coords
+    """
+    nuclei_data = []
+
+    # use cellpose to get nuclei outlines
+    frame_image = overlay_image
+    masks, flows, styles, diams = cellpose_model.eval(
+        frame_image,
+        diameter=0,
+        channels=[1, 3],
+        flow_threshold=0,
+        cellprob_threshold=0.4,
+    )
+    outlines = utils.outlines_list(masks)
+
+    for outline in outlines:
+        centroid = outline.mean(axis=0)
+        nucleus_data = {
+            "Location_Center_X": centroid[0],
+            "Location_Center_Y": centroid[1],
+        }
+        nuclei_data.append(nucleus_data)
+
+    nuclei_data = pd.DataFrame(nuclei_data)
+    return nuclei_data
+
+
+def get_nuclei_locations(
+    DNA_image_path: pathlib.Path, cellpose_model: models.Cellpose
+) -> pd.DataFrame:
+    """finds center X,Y of nuclei and saves as tsv file
+
+    Args:
+        DNA_image_path (pathlib.Path): path to DNA channel image
+        cellpose_model (models.Cellpose): cellpose model for segmenting nuclei
+
+    Returns:
+        pd.DataFrame: dataframe with nuclei center coords
+    """
+    nuclei_data = []
+
+    # use cellpose to get nuclei outlines
+    frame_image = io.imread(DNA_image_path)
+    masks, flows, styles, diams = cellpose_model.eval(
+        frame_image, diameter=80, channels=[0, 0], flow_threshold=0
+    )
+    outlines = utils.outlines_list(masks)
+
+    for outline in outlines:
+        centroid = outline.mean(axis=0)
+        nucleus_data = {
+            "Location_Center_X": centroid[0],
+            "Location_Center_Y": centroid[1],
+        }
+        nuclei_data.append(nucleus_data)
+
+    nuclei_data = pd.DataFrame(nuclei_data)
+    return nuclei_data
+
+
+def segment_cell_health(
+    data_path: pathlib.Path,
+    save_path: pathlib.Path,
+    cellpose_model_DNA: models.Cellpose,
+    cellpose_model_cyto: models.Cellpose,
+):
+    """segments cell health data from data_path and save segmentation data in save_path using cellpose_model
+
+    Args:
+        data_path (pathlib.Path): load path for cell health data
+        save_path (pathlib.Path): save path for segmentation data
+        cellpose_model_DNA (models.Cellpose): cell pose model to use for segmenting DNA
+        cellpose_model_actin (models.Cellpose): cell pose model to use for segmentating actin
+    """
+
+    for plate_path in data_path.iterdir():
+        print(f"Segmenting plate {plate_path.name}")
+        for image_folder in plate_path.iterdir():
+            for image_file in image_folder.iterdir():
+                if ".tiff" in image_file.name:
+                    if "-ch1" in image_file.name:
+                        # segment nuclei
+                        nuc_save_path = str(image_file).replace(
+                            "cell-health", "cell-health-segmented"
+                        )
+                        nuc_save_path = nuc_save_path.replace(".tiff", "-segmented.tsv")
+                        nuc_save_path = pathlib.Path(nuc_save_path)
+
+                        if not nuc_save_path.is_file():
+                            print(f"Segmenting {nuc_save_path.name}")
+                            nuc_save_path.parents[0].mkdir(parents=True, exist_ok=True)
+                            nuc_locations = get_nuclei_locations(
+                                image_file, cellpose_model_DNA
+                            )
+                            nuc_locations.to_csv(nuc_save_path, sep="\t")
+                        else:
+                            print(f"{nuc_save_path.name} already exists!")
+
+                        # segment cytoplasm
+                        current_image = nuc_save_path.name.split("-")[0]
+                        cyto_save_path = f"{nuc_save_path.parents[0]}/{current_image}-cyto-segmented.tsv"
+                        cyto_save_path = pathlib.Path(cyto_save_path)
+
+                        if not cyto_save_path.is_file():
+                            print(f"Segmenting {cyto_save_path.name}")
+                            overlay_image = overlay_channels(
+                                current_image, image_folder
+                            )
+                            cyto_locations = get_cytoplasm_locations(
+                                overlay_image, cellpose_model_cyto
+                            )
+                            cyto_locations.to_csv(cyto_save_path, sep="\t")
+                        else:
+                            print(f"{cyto_save_path.name} already exists!")
+
+
+# In[4]:
+
+
+data_path = pathlib.Path(
+    "/media/roshankern/63af2010-c376-459e-a56e-576b170133b6/data/cell-health/"
+)
+save_path = pathlib.Path(
+    "/media/roshankern/63af2010-c376-459e-a56e-576b170133b6/data/cell-health-segmented/"
+)
+cellpose_model_DNA = models.Cellpose(gpu=True, model_type="cyto")
+cellpose_model_cyto = models.Cellpose(gpu=True, model_type="cyto")
+
+segment_cell_health(data_path, save_path, cellpose_model_DNA, cellpose_model_cyto)
+
