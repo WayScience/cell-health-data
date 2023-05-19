5a8e8b0c115e50a2d742ed7a09d455da4ba967a3
roshankern <roshan.kern@ucdenver.edu>
2023-05-19 16:43:21 -0600

    finish cp feature extraction

--- 2.extract-features/2a.extract-cp-features.sh
+++ 2.extract-features/2a.extract-cp-features.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # convert notebook to python script
-jupyter nbconvert --to python CP-feature-extraction/extract_cp_features.ipynb
+jupyter nbconvert --to python CP-feature-extraction/extract-cp-features.ipynb
 
 # run converted python script
-python CP-feature-extraction/extract_cp_features.py
+python CP-feature-extraction/extract-cp-features.py
