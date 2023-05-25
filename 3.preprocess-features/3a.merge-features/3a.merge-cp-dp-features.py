1f98a836d43f371f15df9677eca1268c58fa260d
Roshan Kern <105665675+roshankern@users.noreply.github.com>
2023-05-25 10:46:12 -0600

    Refactor preprocess features (#15)
    * merge features
    
    * update module
    
    * update readme
    
    * add util docs
    
    * delete norm utils
    
    * update module
    
    * delete unused files
    
    * update module
    
    * update module
    
    * format module
    
    * formatting
    
    * use low memory
    
    * Update 3.preprocess-features/3a.merge-features/merge-utils.py
    
    Co-authored-by: Erik Serrano <31600622+axiomcura@users.noreply.github.com>
    
    * eric suggestions
    
    * eric comments
    
    ---------
    
    Co-authored-by: Erik Serrano <31600622+axiomcura@users.noreply.github.com>

---
+++ 3.preprocess-features/3a.merge-features/3a.merge-cp-dp-features.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Import Libraries
+# 
+
+# In[1]:
+
+
+import pathlib
+import math
+import uuid
+import importlib
+import itertools
+
+import pandas as pd
+from pycytominer.cyto_utils import DeepProfiler_processing
+
+merge_utils = importlib.import_module("merge-utils")
+
+
+# ### Set Load/Save Paths
+# 
+
+# In[2]:
+
+
+# paths to load features/index from
+cp_features_save_path = pathlib.Path(
+    "/media/roshankern/63af2010-c376-459e-a56e-576b170133b6/data/cell-health-nuc-CP/"
+)
+dp_features_save_path = pathlib.Path(
+    "/media/roshankern/63af2010-c376-459e-a56e-576b170133b6/data/cell-health-nuc-DP/outputs/efn_pretrained/features"
+)
+dp_index_path = pathlib.Path(
+    "/media/roshankern/63af2010-c376-459e-a56e-576b170133b6/data/cell-health-nuc-DP/inputs/metadata/index.csv"
+)
+
+# path to save merged features to
+merged_features_save_path = pathlib.Path(
+    "/media/roshankern/63af2010-c376-459e-a56e-576b170133b6/data/cell-health-nuc-merged/"
+)
+merged_features_save_path.mkdir(parents=True, exist_ok=True)
+
+
+# ### Merge Features
+# 
+
+# In[3]:
+
+
+# loop through each plate in CP output, find corresponding DP features, and merge these single-cell features!
+for cp_output_path in cp_features_save_path.iterdir():
+    plate = cp_output_path.name
+    print(f"Merging plate {plate} ...")
+
+    # list where all merged image data will compiled
+    merged_plate_single_cells = []
+
+    # load single cell dataframe for CP data
+    print("Loading CP features...")
+    cp_plate_single_cells = merge_utils.load_cp_feature_data(cp_output_path, plate)
+
+    # load single cell dataframe for DP data
+    print("Loading DP features...")
+    deep_data = DeepProfiler_processing.DeepProfilerData(
+        dp_index_path, dp_features_save_path, filename_delimiter="/"
+    )
+    # get dp index for plate we are interested in
+    deep_data.index_df = deep_data.index_df.loc[
+        deep_data.index_df["Metadata_Plate"] == plate
+    ]
+    # convert site to int (instead of string beginning with 0) so DeepProfiler_processing can find output
+    deep_data.index_df["Metadata_Site"] = pd.to_numeric(
+        deep_data.index_df["Metadata_Site"]
+    )
+    deep_single_cell = DeepProfiler_processing.SingleCellDeepProfiler(deep_data)
+    dp_plate_single_cells = deep_single_cell.get_single_cells(output=True)
+
+    # iterate through each image in the plate (unique image for each plate, well, site combination)
+    print("Merging features...")
+    wells = dp_plate_single_cells["Metadata_Well"].unique()
+    sites = dp_plate_single_cells["Metadata_Site"].unique()
+
+    for well, site in itertools.product(wells, sites):
+
+        # find single cell data for the well, site combination
+        cp_image_single_cells = cp_plate_single_cells.loc[
+            (cp_plate_single_cells["Metadata_Well"] == well)
+            & (cp_plate_single_cells["Metadata_Site"] == site)
+        ]
+        dp_image_single_cells = dp_plate_single_cells.loc[
+            (dp_plate_single_cells["Metadata_Well"] == well)
+            & (dp_plate_single_cells["Metadata_Site"] == site)
+        ]
+
+        # get the merged single-cell image data and add this to the merged plate data
+        merged_image_data = merge_utils.merge_CP_DP_image_data(
+            cp_image_single_cells, dp_image_single_cells
+        )
+        merged_plate_single_cells.append(merged_image_data)
+
+    # combine all merged image data into one dataframe for the entire plate
+    merged_plate_single_cells = pd.concat(merged_plate_single_cells).reset_index(
+        drop=True
+    )
+
+    # compress and save merged single-cell data
+    print(f"Saving merged features...")
+    merged_plate_single_cells_save_path = pathlib.Path(
+        f"{merged_features_save_path}/{plate}-merged-single-cell.csv.gz"
+    )
+    merged_plate_single_cells.to_csv(
+        merged_plate_single_cells_save_path, compression="gzip", index=False
+    )
+
---
+++ 3.preprocess-features/3b.normalize-features/3b.normalize-merged-features.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Import Libraries
+# 
+
+# In[1]:
+
+
+import pathlib
+import joblib
+import importlib
+
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from pycytominer.cyto_utils import DeepProfiler_processing
+
+normalization_utils = importlib.import_module("normalization-utils")
+
+
+# ### Set Load/Save Paths
+# 
+
+# In[2]:
+
+
+# paths to load merged features,index, and annotations from
+merged_features_save_path = pathlib.Path(
+    "/media/roshankern/63af2010-c376-459e-a56e-576b170133b6/data/cell-health-nuc-merged/"
+)
+dp_index_path = pathlib.Path(
+    "/media/roshankern/63af2010-c376-459e-a56e-576b170133b6/data/cell-health-nuc-DP/inputs/metadata/index.csv"
+)
+annotations_path = pathlib.Path(
+    "../0.image-download/manifest/idr0080-screenA-annotation.csv"
+)
+
+# path to save normalized merged features to
+normalized_merged_features_save_path = pathlib.Path(
+    "/media/roshankern/63af2010-c376-459e-a56e-576b170133b6/data/cell-health-nuc-merged-normalized/"
+)
+normalized_merged_features_save_path.mkdir(parents=True, exist_ok=True)
+scaler_save_dir = pathlib.Path("normalization-scalers/")
+scaler_save_dir.mkdir(parents=True, exist_ok=True)
+
+
+# ### Normalize merged single-cell data
+# 
+
+# In[3]:
+
+
+# iterate through each merged plate, find normalization scaler from negative control cells, and apply this scaler to all cells
+for merged_single_cell_plate_path in merged_features_save_path.iterdir():
+    # get only plate name from merged features file name
+    plate = merged_single_cell_plate_path.name.split("-")[0]
+    print(f"Normalizing plate {plate}...")
+
+    # load in one row to create datatypes dictionary for faster loading
+    first_row_data = pd.read_csv(
+        merged_single_cell_plate_path, compression="gzip", nrows=1
+    )
+    metadata_cols = [col for col in first_row_data.columns if "P__" not in col]
+    feature_cols = [col for col in first_row_data.columns if "P__" in col]
+
+    # specify datatypes for metadata/feature columns
+    metadata_dtypes = {metadata_col: str for metadata_col in metadata_cols}
+    feature_dtypes = {feature_col: np.float32 for feature_col in feature_cols}
+    # combine both dictionaries
+    plate_dtypes = {**metadata_dtypes, **feature_dtypes}
+
+    # load plate single-cell data
+    print(f"Loading single-cell data...")
+    plate_merged_single_cells = pd.read_csv(
+        merged_single_cell_plate_path,
+        compression="gzip",
+        dtype=plate_dtypes,
+        low_memory=True,
+    )
+
+    # create per-plate normalization scaler from the normalization population
+    print(f"Deriving normalization scaler...")
+    plate_scaler = normalization_utils.get_normalization_scaler(
+        plate_merged_single_cells
+    )
+    # save normalization scaler
+    scaler_save_path = pathlib.Path(
+        f"{scaler_save_dir}/{plate}-merged-normalization-scaler.joblib"
+    )
+    joblib.dump(plate_scaler, scaler_save_path)
+
+    # apply scaler to all single cell feature data
+    print(f"Applying normalization scaler...")
+    # get normalized feature data
+    feature_cols = [
+        col for col in plate_merged_single_cells.columns.to_list() if "P__" in col
+    ]
+    features = plate_merged_single_cells[feature_cols].values
+    features = plate_scaler.transform(features)
+    features = pd.DataFrame(features, columns=feature_cols)
+    # get metadata for all single cells
+    metadata_cols = [
+        col for col in plate_merged_single_cells.columns.to_list() if "P__" not in col
+    ]
+    metadata = plate_merged_single_cells[metadata_cols]
+
+    # combine metadata and normalized features for all single cells (replace other single cell dataframe to not keep two dfs in memory)
+    plate_merged_single_cells = pd.concat([metadata, features], axis=1)
+
+    # compress and save merged single-cell data
+    print(f"Saving normalized features...")
+    normalized_merged_plate_single_cells_save_path = pathlib.Path(
+        f"{normalized_merged_features_save_path}/{plate}-normalized-merged-single-cell.csv.gz"
+    )
+    plate_merged_single_cells.to_csv(
+        normalized_merged_plate_single_cells_save_path, compression="gzip", index=False
+    )
+
