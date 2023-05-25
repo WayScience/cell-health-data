50190a817f24e59f61020f115b3f32e13567f00c
roshankern <roshan.kern@ucdenver.edu>
2023-05-25 08:37:48 -0600

    format module

---
+++ 3.preprocess-features/3b.normalize-features/3b.normalize-merged-features.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Import Libraries
+# 
+
+# In[1]:
+
+
+import pathlib
+import joblib
+import importlib
+
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+from pycytominer.cyto_utils import DeepProfiler_processing
+
+normalization_utils = importlib.import_module("normalization-utils")
+
+
+# ### Set Load/Save Paths
+# 
+
+# In[2]:
+
+
+# paths to load merged features,index, and annotations from
+merged_features_save_path = pathlib.Path(
+    "/media/roshankern/63af2010-c376-459e-a56e-576b170133b6/data/cell-health-nuc-merged/"
+)
+dp_index_path = pathlib.Path(
+    "/media/roshankern/63af2010-c376-459e-a56e-576b170133b6/data/cell-health-nuc-DP/inputs/metadata/index.csv"
+)
+annotations_path = pathlib.Path(
+    "../0.image-download/manifest/idr0080-screenA-annotation.csv"
+)
+
+# path to save normalized merged features to
+normalized_merged_features_save_path = pathlib.Path(
+    "/media/roshankern/63af2010-c376-459e-a56e-576b170133b6/data/cell-health-nuc-merged-normalized/"
+)
+normalized_merged_features_save_path.mkdir(parents=True, exist_ok=True)
+scaler_save_dir = pathlib.Path("normalization-scalers/")
+scaler_save_dir.mkdir(parents=True, exist_ok=True)
+
+
+# ### Normalize merged single-cell data
+# 
+
+# In[3]:
+
+
+for merged_single_cell_plate_path in merged_features_save_path.iterdir():
+    # get only plate name from merged features file name
+    plate = merged_single_cell_plate_path.name.split("-")[0]
+    print(f"Normalizing plate {plate}...")
+
+    # load plate single-cell data
+    print(f"Loading single-cell data...")
+    plate_merged_single_cells = pd.read_csv(merged_single_cell_plate_path, compression="gzip", engine="pyarrow")
+
+    # create per-plate normalization scaler from the normalization population
+    print(f"Deriving normalization scaler...")
+    plate_scaler = normalization_utils.get_normalization_scaler(
+        plate_merged_single_cells
+    )
+    # save normalization scaler
+    scaler_save_path = pathlib.Path(
+        f"{scaler_save_dir}/{plate}-merged-normalization-scaler.joblib"
+    )
+    joblib.dump(plate_scaler, scaler_save_path)
+
+    # apply scaler to all single cell feature data
+    print(f"Applying normalization scaler...")
+    # get normalized feature data
+    feature_cols = [
+        col for col in plate_merged_single_cells.columns.to_list() if "P__" in col
+    ]
+    features = plate_merged_single_cells[feature_cols].values
+    features = plate_scaler.transform(features)
+    features = pd.DataFrame(features, columns=feature_cols)
+    # get metadata for all single cells
+    metadata_cols = [
+        col for col in plate_merged_single_cells.columns.to_list() if "P__" not in col
+    ]
+    metadata = plate_merged_single_cells[metadata_cols]
+
+    # combine metadata and normalized features for all single cells (replace other single cell dataframe to not keep two dfs in memory)
+    plate_merged_single_cells = pd.concat([metadata, features], axis=1)
+
+    # compress and save merged single-cell data
+    print(f"Saving normalized features...")
+    normalized_merged_plate_single_cells_save_path = pathlib.Path(f"{normalized_merged_features_save_path}/{plate}-normalized-merged-single-cell.csv.gz")
+    plate_merged_single_cells.to_csv(normalized_merged_plate_single_cells_save_path, compression="gzip", index=False)
+
