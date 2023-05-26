7e8aa1d93aa21a9076dc63b4e273dd4e5c0dfea0
Roshan Kern <105665675+roshankern@users.noreply.github.com>
2023-05-26 14:06:37 -0600

    Refactor classification module (#16)
    * update module
    
    * update classification notebook
    
    * update classification module
    
    * jenna suggestions
    
    * first plate norm scaler
    
    * upload scalers

--- 4.classify-single-cell-phenotypes/classification-utils.py
+++ 4.classify-single-cell-phenotypes/classification-utils.py
@@ -8,70 +8,47 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 
 
-def classify_plate_cells(classifier: LogisticRegression, plate_load_path: pathlib.Path) -> pd.DataFrame:
+def get_probas_dataframe(plate_features: pd.DataFrame, model: LogisticRegression, feature_type: str) -> pd.DataFrame:
     """
-    classify cells in plate
+    Get probabilites for plate features from a phenotypic classification model
 
     Parameters
     ----------
-    classifier :
-        sklearn LogisticRegression classifier
-    plate_load_path : pathlib.Path
-        path to plate to load and classify
+    plate_features : pd.DataFrame
+        plate features to classify
+    model : LogisticRegression
+        model to use for plate feature classification
+    feature_type : str
+        type of features to use for classification.
+        CP, DP, or CP_and_DP
 
     Returns
     -------
-    plate_classifications : pd.DataFrame
-        single cell dataframe with metadata and phenotypic class probabilities
+    pd.DataFrame
+        dataframe with single-cell probabilities for classes from given model
     """
 
-    # load in one row to create datatypes dictionary for faster loading
-    first_row_data = pd.read_csv(plate_load_path, compression="gzip", nrows=1)
-    metadata_cols = [col for col in first_row_data.columns if "efficientnet" not in col]
-    feature_cols = [col for col in first_row_data.columns if "efficientnet" in col]
+    # determine which feature columns should be loaded depending on feature type
+    # if there is no "and" we can use feature type as prefix
+    if "and" not in feature_type:
+        cols_to_load = [
+            col
+            for col in plate_features.columns.to_list()
+            if f"{feature_type}__" in col
+        ]
+    # if there is an "and" we should all features (which all have "P__" prefix)
+    else:
+        cols_to_load = [
+            col for col in plate_features.columns.to_list() if "P__" in col
+        ]
 
-    # specify datatypes for metadata/feature columns
-    metadata_dtypes = {metadata_col: str for metadata_col in metadata_cols}
-    feature_dtypes = {feature_col: np.float32 for feature_col in feature_cols}
-    # combine both dictionaries
-    plate_dtypes = {**metadata_dtypes, **feature_dtypes}
-
-    # load in csv with the specified column names/datatypes
-    plate_data = pd.read_csv(
-        plate_load_path, compression="gzip", dtype=plate_dtypes, low_memory=True
-    )
-
-    # combine metadatas and classifications dataframes
-    cell_metadatas = plate_data[metadata_cols].reset_index(drop=True)
-    cell_features = plate_data[feature_cols].values
-    cell_classifications = pd.DataFrame(
-        classifier.predict_proba(cell_features),
-        columns=classifier.classes_,
+    # load these particular features and get the values
+    single_cell_features = plate_features[cols_to_load].values
+    
+    # get and return the predicted probabilities
+    probas_dataframe = pd.DataFrame(
+        model.predict_proba(single_cell_features),
+        columns=model.classes_,
     ).reset_index(drop=True)
-    return pd.concat([cell_metadatas, cell_classifications], axis=1)
-
-
-def save_feature_classifications(phenotypic_profiling_model: LogisticRegression, normalized_plates_path: pathlib.Path, output_dir: pathlib.Path):
-    """
-    classify features from normalized plates and save these classifications
-
-    Parameters
-    ----------
-    phenotypic_profiling_model : LogisticRegression
-        model to use for classification
-    normalized_plates_path : pathlib.Path
-        path to normalized plates
-    output_dir : pathlib.Path
-        where to save features classifications
-    """
     
-    output_dir.mkdir(exist_ok=True, parents=True)
-    
-    # classify cells from each plate and save classifications
-    for plate_load_path in normalized_plates_path.iterdir():
-        print(f"Classifying feature data for {plate_load_path.name}...")
-        plate = plate_load_path.name.strip("_normalized_single_cell.csv.gz")
-        plate_classification_save_path = pathlib.Path(f"{output_dir}/{plate}_cell_classifications.csv.gz")
-        
-        plate_classifications = classify_plate_cells(phenotypic_profiling_model, plate_load_path)
-        plate_classifications.to_csv(plate_classification_save_path, compression = "gzip")
+    return probas_dataframe
